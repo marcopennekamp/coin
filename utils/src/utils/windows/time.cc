5766c19c0ef78f7d7f646ef161db931874860256
Marco Pennekamp <marco.pennekamp96@gmail.com>
2013-02-22 16:06:35 +0100

    Cleaned up project files.
    Renamed directory_windows to directory.
    Removed const from TimeNanoseconds.

--- utils/src/utils/windows/time.cc
+++ utils/src/utils/windows/time.cc
@@ -26,7 +26,7 @@ void TimeInit () {
     // TODO Throw error!
 }
 
-const Time TimeNanoseconds () {
+Time TimeNanoseconds () {
     LARGE_INTEGER time;
     QueryPerformanceCounter (&time);
     return (u64) (time.QuadPart / frequency_nanoseconds);
